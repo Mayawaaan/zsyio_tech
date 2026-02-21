b61e8b8387b1d6622cdd27ee959f29d228d07085
Ayan Magardey <91563271+Mayawaaan@users.noreply.github.com>
2026-02-21 23:01:45 +0530

    Update API base URL to include '/api/' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const ACCESS_TOKEN = 'access_token';
 export const REFRESH_TOKEN = 'refresh_token';
 
-const API_BASE_URL = "https://zsyio-tech-67m6.onrender.com" || import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = 'https://zsyio-tech-67m6.onrender.com/api/' || import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
